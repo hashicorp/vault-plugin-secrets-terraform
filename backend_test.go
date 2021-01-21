8bef710b22930af51178c73e5a6ea41b13f8750e
catsby <clint@ctshryock.com>
2021-01-21 14:33:25 -0600

    move things around

--- backend_test.go
+++ backend_test.go
@@ -2,15 +2,23 @@ package tfc
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/stretchr/testify/assert"
 )
 
 const (
-	defaultLeaseTTLHr = 1
-	maxLeaseTTLHr     = 12
+	defaultLeaseTTLHr           = 1
+	maxLeaseTTLHr               = 12
+	envVarRunAccTests           = "VAULT_ACC"
+	envVarTerraformToken        = "TEST_TF_TOKEN"
+	envVarTerraformOrganization = "TEST_TF_ORGANIZATION"
+	envVarTerraformTeamID       = "TEST_TF_TEAM_ID"
+	envVarTerraformUserID       = "TEST_TF_USER_ID"
 )
 
 func getTestBackend(tb testing.TB) (*tfBackend, logical.Storage) {
@@ -27,4 +35,173 @@ func getTestBackend(tb testing.TB) (*tfBackend, logical.Storage) {
 	}
 
 	return b.(*tfBackend), config.StorageView
+}
+
+var runAcceptanceTests = os.Getenv(envVarRunAccTests) == "1"
+
+type testEnv struct {
+	Token        string
+	Organization string
+	TeamID       string
+	UserID       string
+
+	Backend logical.Backend
+	Context context.Context
+	Storage logical.Storage
+
+	// SecretToken tracks the API token, for checking rotations
+	SecretToken string
+
+	// TokenIDs tracks the IDs of generated tokens, to make sure we clean up
+	TokenIDs []string
+}
+
+func (e *testEnv) AddConfig(t *testing.T) {
+	req := &logical.Request{
+		Operation: logical.CreateOperation,
+		Path:      "config",
+		Storage:   e.Storage,
+		Data: map[string]interface{}{
+			"token": e.Token,
+		},
+	}
+	resp, err := e.Backend.HandleRequest(e.Context, req)
+	assert.False(t, (err != nil || (resp != nil && resp.IsError())), fmt.Sprintf("bad: resp: %#v\nerr:%v", resp, err))
+	assert.Nil(t, resp)
+}
+
+func (e *testEnv) AddOrgTokenRole(t *testing.T) {
+	req := &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "role/test-org-token",
+		Storage:   e.Storage,
+		Data: map[string]interface{}{
+			"organization": e.Organization,
+		},
+	}
+	resp, err := e.Backend.HandleRequest(e.Context, req)
+	assert.False(t, (err != nil || (resp != nil && resp.IsError())), fmt.Sprintf("bad: resp: %#v\nerr:%v", resp, err))
+}
+
+func (e *testEnv) ReadOrgToken(t *testing.T) {
+	req := &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "creds/test-org-token",
+		Storage:   e.Storage,
+	}
+	resp, err := e.Backend.HandleRequest(e.Context, req)
+	assert.False(t, (err != nil || (resp != nil && resp.IsError())), fmt.Sprintf("bad: resp: %#v\nerr:%v", resp, err))
+	assert.NotNil(t, resp)
+	assert.NotEmpty(t, resp.Data["token"])
+
+	if t, ok := resp.Data["token"]; ok {
+		e.SecretToken = t.(string)
+	}
+	// verify there is a token
+	b := e.Backend.(*tfBackend)
+	client, err := b.getClient(e.Context, e.Storage)
+	if err != nil {
+		t.Fatal("fatal getting client")
+	}
+	ot, err := client.OrganizationTokens.Read(e.Context, e.Organization)
+	if err != nil {
+		t.Fatalf("unexpected error reading organization token: %s", err)
+	}
+	assert.NotNil(t, ot)
+}
+
+func (e *testEnv) AddTeamTokenRole(t *testing.T) {
+	req := &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "role/test-team-token",
+		Storage:   e.Storage,
+		Data: map[string]interface{}{
+			"organization": e.Organization,
+			"team_id":      e.TeamID,
+		},
+	}
+	resp, err := e.Backend.HandleRequest(e.Context, req)
+	assert.False(t, (err != nil || (resp != nil && resp.IsError())), fmt.Sprintf("bad: resp: %#v\nerr:%v", resp, err))
+}
+
+func (e *testEnv) ReadTeamToken(t *testing.T) {
+	req := &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "creds/test-team-token",
+		Storage:   e.Storage,
+	}
+	resp, err := e.Backend.HandleRequest(e.Context, req)
+	assert.False(t, (err != nil || (resp != nil && resp.IsError())), fmt.Sprintf("bad: resp: %#v\nerr:%v", resp, err))
+	assert.NotNil(t, resp)
+	assert.NotEmpty(t, resp.Data["token"])
+
+	// verify there is a token
+	b := e.Backend.(*tfBackend)
+	client, err := b.getClient(e.Context, e.Storage)
+	if err != nil {
+		t.Fatal("fatal getting client")
+	}
+	tt, err := client.TeamTokens.Read(e.Context, e.TeamID)
+	if err != nil {
+		t.Fatalf("unexpected error reading team token: %s", err)
+	}
+	assert.NotNil(t, tt)
+	if t, ok := resp.Data["token"]; ok {
+		e.SecretToken = t.(string)
+	}
+}
+
+func (e *testEnv) AddUserTokenRole(t *testing.T) {
+	req := &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "role/test-user-token",
+		Storage:   e.Storage,
+		Data: map[string]interface{}{
+			"user_id": e.UserID,
+		},
+	}
+	resp, err := e.Backend.HandleRequest(e.Context, req)
+	assert.False(t, (err != nil || (resp != nil && resp.IsError())), fmt.Sprintf("bad: resp: %#v\nerr:%v", resp, err))
+}
+
+func (e *testEnv) ReadUserToken(t *testing.T) {
+	req := &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "creds/test-user-token",
+		Storage:   e.Storage,
+	}
+	resp, err := e.Backend.HandleRequest(e.Context, req)
+	assert.False(t, (err != nil || (resp != nil && resp.IsError())), fmt.Sprintf("bad: resp: %#v\nerr:%v", resp, err))
+	assert.NotNil(t, resp)
+	if t, ok := resp.Data["token_id"]; ok {
+		e.TokenIDs = append(e.TokenIDs, t.(string))
+	}
+	assert.NotEmpty(t, resp.Data["token"])
+
+	if e.SecretToken != "" {
+		assert.NotEqual(t, e.SecretToken, resp.Data["token"])
+	}
+
+	// collect secret IDs to revoke at end of test
+	assert.NotNil(t, resp.Secret)
+	if t, ok := resp.Secret.InternalData["token_id"]; ok {
+		e.SecretToken = t.(string)
+	}
+}
+
+func (e *testEnv) RevokeUserTokens(t *testing.T) {
+	if len(e.TokenIDs) == 0 {
+		t.Fatalf("expected 2 tokens, got: %d", len(e.TokenIDs))
+	}
+
+	for _, id := range e.TokenIDs {
+		b := e.Backend.(*tfBackend)
+		client, err := b.getClient(e.Context, e.Storage)
+		if err != nil {
+			t.Fatal("fatal getting client")
+		}
+		if err := client.UserTokens.Delete(e.Context, id); err != nil {
+			t.Fatalf("unexpected error reading organization token: %s", err)
+		}
+	}
 }
